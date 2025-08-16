0bd3968ce7b9ccd385d00dc708d811412b0a47a9
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-08-16 21:01:42 +0600

    loop problem

---
+++ Basic-js/Conditions.js
@@ -0,0 +1,15 @@
+// if ([] == ![]) {
+//   console.log(1);
+// } else {
+//   console.log(2);
+// }
+
+// if (NaN == NaN) {
+//   console.log(1);
+// } else {
+//   console.log(2);
+// }
+
+// if ('5' + 1 == 51) console.log(1); else console.log(2);
+if (null == undefined) console.log(1); else console.log(2);
+
---
+++ Basic-js/Loop.js
@@ -0,0 +1,5 @@
+// for, while, do while
+// for (let i = 0; i < 101; i++) {
+//   console.log(i);
+// }
+
