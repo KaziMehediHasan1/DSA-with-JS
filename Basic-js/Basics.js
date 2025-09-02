42f4365eec2d7c55a657e664978067b6eb27d39a
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-09-02 23:57:46 +0600

    solved basic looping prolems

---
+++ Basic-js/Basics.js
@@ -0,0 +1,36 @@
+// Log all odd numbers from 1 to 20
+
+// for (let index = 1; index < 20; index++) {
+//   console.log(index, "checking");
+//   if (index % 2 === 1) {
+//     console.log("odd number", index);
+//   }
+// }
+
+
+// Log all even numbers from 1 to 20
+
+// for (let index = 1; index < 20; index++) {
+//   console.log(index, "checking");
+//   if (index % 2 === 0) {
+//     console.log("even number", index);
+//   }
+// }
+
+// 1 - 100 sum of all numbers
+
+// let sum = 0;
+// for (let i = 1; i < 100; i++) {
+//   sum += i;
+// }
+// console.log(sum,"check all sum")
+
+// Reverse a string using a loop
+
+// let names = "kazi mehedi hasan";
+// for (let lenghtOfName = names.length - 1; lenghtOfName >= 0; lenghtOfName--) {
+//   const change = names[lenghtOfName];
+//   console.log(change, "reverse checking");
+// }
+
+
